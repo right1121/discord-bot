3983b40a15725cc85bf36bec98419e5e65c2eb22
MsakiHoshino <right1121@hotmail.co.jp>
2022-11-04 23:27:40 +0900

    型追加

--- event/voiceStateUpdate.js
+++ event/voiceStateUpdate.js
@@ -16,6 +16,8 @@ export const isAllowSendTime = (currentDate = getJstDate()) => {
 
 /**
  * メッセージの送信
+ * @param {import('discord.js').Client} client
+ * @param {string} client
  */
 export const sendMessage = (client, text) => {
   if (!isAllowSendTime()) return
@@ -33,42 +35,52 @@ export const isTargetChannel = (guildMember) => {
 /**
  * ユーザーがVoiceチャンネルに接続したか
  * VC: VoiceChanel
+ * @param {import('discord.js').VoiceState} oldState
+ * @param {import('discord.js').VoiceState} newState
  */
-export const isConnectionVC = (oldGuildMember, newGuildMember) => {
-  return oldGuildMember.channelId === null && newGuildMember.channelId !== null
+export const isConnectionVC = (oldState, newState) => {
+  return oldState.channelId === null && newState.channelId !== null
 }
 
 /**
  * ユーザーが初めての通話かどうか
+ * @param {import('discord.js').Client} client
+ * @param {import('discord.js').VoiceState} newState
  */
-export const isStartCall = (client, newGuildMember) => {
-  return client.channels.cache.get(newGuildMember.channelId).members.size === 1
+export const isStartCall = (client, newState) => {
+  return client.channels.cache.get(newState.channelId).members.size === 1
 }
 
 /**
  * 通話接続時
+ * @param {import('discord.js').Client} client
+ * @param {import('discord.js').VoiceState} newState
  */
-export const connectionVC = (client, newGuildMember) => {
-  if (isTargetChannel(newGuildMember)) {
-    if (isStartCall(client, newGuildMember)) {
-      sendMessage(client, `<@${newGuildMember.id}>が通話を開始しました。\n`)
+export const connectionVC = (client, newState) => {
+  if (isTargetChannel(newState)) {
+    if (isStartCall(client, newState)) {
+      sendMessage(client, `<@${newState.id}>が通話を開始しました。\n`)
     }
   }
 }
 
 /**
  * ユーザーがVoiceチャンネルから切断したか
+ * @param {import('discord.js').VoiceState} oldState
+ * @param {import('discord.js').VoiceState} newState
  */
-export const isDisconnectionVC = (oldGuildMember, newGuildMember) => {
-  return oldGuildMember.channelId !== null && newGuildMember.channelId === null
+export const isDisconnectionVC = (oldState, newState) => {
+  return oldState.channelId !== null && newState.channelId === null
 }
 
 /**
  * 通話切断時
+ * @param {import('discord.js').Client} client
+ * @param {import('discord.js').VoiceState} oldState
  */
-export const disconnectionVC = (client, oldGuildMember) => {
-  if (isTargetChannel(oldGuildMember)) {
-    if (isEndCall(client, oldGuildMember)) {
+export const disconnectionVC = (client, oldState) => {
+  if (isTargetChannel(oldState)) {
+    if (isEndCall(client, oldState)) {
       sendMessage(client, '通話が終了しました。\n')
     }
   }
@@ -76,31 +88,35 @@ export const disconnectionVC = (client, oldGuildMember) => {
 
 /**
  * Voiceチャンネルの通話が終了したか
+ * @param {import('discord.js').Client} client
+ * @param {import('discord.js').VoiceState} oldState
  */
-export const isEndCall = (client, oldGuildMember) => {
-  return client.channels.cache.get(oldGuildMember.channelId).members.size === 0
+export const isEndCall = (client, oldState) => {
+  return client.channels.cache.get(oldState.channelId).members.size === 0
 }
 
 /**
  * Voiceチャンネル内で移動があったか
+ * @param {import('discord.js').VoiceState} oldState
+ * @param {import('discord.js').VoiceState} newState
  */
-export const isMoveVC = (oldGuildMember, newGuildMember) => {
-  return oldGuildMember.channelId !== null && newGuildMember.channelId !== null
+export const isMoveVC = (oldState, newState) => {
+  return oldState.channelId !== null && newState.channelId !== null
 }
 
 /**
  * Voiceチャンネルに変化があった際の処理
- * @param {*} client
- * @returns
+ * @param {import('discord.js').Client} client
+ * @returns {(oldState: import('discord.js').VoiceState, newState: import('discord.js').VoiceState) => void}
  */
-export const voiceStateUpdate = (client) => (oldGuildMember, newGuildMember) => {
-  if (isConnectionVC(oldGuildMember, newGuildMember)) {
-    connectionVC(client, newGuildMember)
-  } else if (isDisconnectionVC(oldGuildMember, newGuildMember)) {
-    disconnectionVC(client, oldGuildMember)
-  } else if (isMoveVC(oldGuildMember, newGuildMember)) {
+export const voiceStateUpdate = (client) => (oldState, newState) => {
+  if (isConnectionVC(oldState, newState)) {
+    connectionVC(client, newState)
+  } else if (isDisconnectionVC(oldState, newState)) {
+    disconnectionVC(client, oldState)
+  } else if (isMoveVC(oldState, newState)) {
     // チャンネル移動
   } else {
-    console.error('想定外のパラメータ => ', 'oldGuildMember: ', oldGuildMember, 'newGuildMember: ', newGuildMember)
+    console.error('想定外のパラメータ => ', 'oldState: ', oldState, 'newState: ', newState)
   }
 }
